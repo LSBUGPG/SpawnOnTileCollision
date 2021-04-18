f8c22224d93b0b947476381e6a18c72796ab8f0e
Paul Sinnett <paul.sinnett@gmail.com>
2021-04-19 00:15:10 +0100

    Initial commit

---
+++ Assets/Ground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu()]
+public class Ground : TileBase
+{
+    public GameObject prefab;
+
+    public override void GetTileData(Vector3Int position, ITilemap map, ref TileData data)
+    {
+        base.GetTileData(position, map, ref data);
+        Debug.Log($"{data.sprite}");
+        data.gameObject = prefab;
+        Debug.Log($"{data.gameObject}");
+    }
+}
---
+++ Assets/Move.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    public Rigidbody2D physics;
+    public float force = 10.0f;
+
+    void Update()
+    {
+        physics.AddForce(Vector2.right * Input.GetAxis("Horizontal") * force);
+    }
+}
